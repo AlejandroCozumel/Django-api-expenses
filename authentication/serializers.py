afba59a7bf54f28dc26ae0f79754b1f50e21230e
AlejandroCozumel <alejandroo14@gmail.com>
2022-05-05 20:54:03 -0500

    login agregado

--- authentication/serializers.py
+++ authentication/serializers.py
@@ -1,8 +1,13 @@
+import pdb
 from rest_framework import serializers
 from .models import User
+from django.contrib import auth
+from rest_framework.exceptions import AuthenticationFailed
+from django.core.validators import RegexValidator
 
 class RegisterSerializer(serializers.ModelSerializer):
     password=serializers.CharField(max_length=68, min_length=6, write_only=True)
+    email=serializers.EmailField(max_length=255, min_length=3)
 
     class Meta:
       model=User
@@ -25,3 +30,37 @@ class EmailVerificationSerializer(serializers.Serializer):
   class Meta:
     model=User
     fields = ['token']
+
+class LoginSerializer(serializers.ModelSerializer):
+  email=serializers.EmailField(max_length=255, min_length=3, read_only=True)
+  username=serializers.CharField(max_length=68, min_length=6)  
+  password=serializers.CharField(max_length=68, min_length=6, write_only = True)
+  tokens=serializers.CharField(max_length=68, min_length=6, read_only=True)
+
+  class Meta:
+    model=User
+    fields=['email', 'username', 'password', 'tokens']
+
+  def validate(self, attrs):
+    username=attrs.get('username','')
+    password=attrs.get('password','')
+
+    user=auth.authenticate(username=username, password=password)
+
+    # import pdb
+    # pdb.set_trace()
+    
+    if not user:
+      raise AuthenticationFailed('Invalid credentials, check email, password, and try again')
+
+    if not user.is_verified:
+      raise AuthenticationFailed('User is not verified')
+
+    if not user.is_active:
+      raise AuthenticationFailed('User is blocked, contact admin')
+
+    return {
+      'email': user.email,
+      'username': user.username,
+      'tokens': user.tokens()
+    }
--- authentication/views.py
+++ authentication/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from rest_framework import generics, status, views
-from .serializers import EmailVerificationSerializer, RegisterSerializer
+from .serializers import EmailVerificationSerializer, RegisterSerializer, LoginSerializer
 from rest_framework.response import Response
 from rest_framework_simplejwt.tokens import RefreshToken
 from .models import User
@@ -58,3 +58,11 @@ class VerifyEmail(views.APIView):
       return Response({'error': "Activation link has been expired"}, status=status.HTTP_400_BAD_REQUEST)
     except jwt.exceptions.DecodeError as identifier:
       return Response({'error': "Invalid token"}, status=status.HTTP_400_BAD_REQUEST)
+
+class LoginApiView(generics.GenericAPIView):
+  serializer_class=LoginSerializer
+  def post(self,request):
+    serializer=self.serializer_class(data=request.data)
+    serializer.is_valid(raise_exception=True)
+
+    return Response(serializer.data, status=status.HTTP_200_OK)
